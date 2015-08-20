4d48b7b348961a69d65f4d31e81bd1de896e272c
Denis Tyurkov <dtyurkov@gmail.com>
2015-08-20 17:52:27 +1200

    ADD index.html to Products View

--- shopping_cart/config/routes.rb
+++ shopping_cart/config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  resources :order_products
+  get 'welcome/index'
+
+  resources :orders
   resources :users
-  get 'order/index'
+  resources :products
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
